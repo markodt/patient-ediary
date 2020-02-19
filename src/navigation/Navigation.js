98e19ab7234600dc3f4a96c5716d19f96822e15a
Marko D. Topic <markodominik.topic@silverrailtech.com>
2020-02-19 16:18:22 +0100

    Modify HomeScreen design, add bottom navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { BottomNavigation } from 'react-native-paper';
+import * as data from '../../data.json';
+
+const { alertNumber } = data;
+
+export default class Navigation extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'alerts', title: 'Alerts', icon: 'bell', badge: alertNumber },
+      { key: 'home', title: 'Home', icon: 'home' },
+      { key: 'more', title: 'More', icon: 'menu' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    alerts: () => null,
+    home: () => null,
+    more: () => null,
+  });
+
+  render() {
+    return (
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
+    );
+  }
+}
