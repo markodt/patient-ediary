effbc305a4c6c6998a4bf9366dddd8ee0048cf1f
Marko D. Topic <markodominik.topic@silverrailtech.com>
2020-03-04 14:27:29 +0100

    Create OngoingHeadacheScreen component

--- src/components/OngoingHeadacheScreen.js
+++ src/components/OngoingHeadacheScreen.js
@@ -1,54 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { RadioButton, Button } from 'react-native-paper';
-import * as screens from '../../screens_en.json';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
-export default class OngoingHeadacheScreen extends React.Component {
-  state = {
-    value: null,
-  };
-
-  render() {
-    const { route, navigation } = this.props;
-    const screen = screens[route.name];
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.question}>{screen.text}</Text>
-        <View style={styles.radioButtonContainer}>
-          <RadioButton.Group
-            onValueChange={value => this.setState({ value })}
-            value={this.state.value}
-          >
-            <RadioButton.Item
-              label="Yes"
-              value="yes"
-              style={styles.radioButton}
-            />
-            <RadioButton.Item
-              label="No"
-              value="no"
-              style={styles.radioButton}
-            />
-          </RadioButton.Group>
-        </View>
-        <View style={styles.bottomSection}>
-          <Button
-            mode="contained"
-            uppercase={false}
-            disabled={!this.state.value}
-            onPress={() =>
-              navigation.navigate(
-                this.state.value === 'yes' ? screen.nextYes : screen.nextNo,
-              )
-            }
-          >
-            Next
-          </Button>
-        </View>
+export default function OngoingHeadacheScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.centerContent}>
+        <Image
+          style={styles.headacheImage}
+          source={require('../../images/headache1_big.png')}
+        />
       </View>
-    );
-  }
+      <Text style={[styles.title, styles.centerText]}>Ongoing Headache</Text>
+      <Text style={[styles.noteText, styles.centerText]}>
+        When this headache has ended, please tap the “Finish Headache Diary”
+        button on the home screen.
+      </Text>
+      <View style={styles.bottomSection}>
+        <Button
+          mode="contained"
+          uppercase={false}
+          onPress={() => navigation.navigate('h0')}
+        >
+          Return Home
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,19 +34,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
   },
-  question: {
-    fontSize: 18,
+  centerText: {
+    textAlign: 'center',
+  },
+  centerContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headacheImage: {
+    width: 160,
+    height: 160,
+    marginTop: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#212121',
-    margin: 30,
+    marginBottom: 20,
   },
-  radioButtonContainer: {
-    marginHorizontal: 30,
-    borderTopColor: '#979797',
-    borderTopWidth: 1,
-  },
-  radioButton: {
-    borderBottomColor: '#979797',
-    borderBottomWidth: 1,
+  noteText: {
+    fontSize: 16,
+    color: '#757575',
+    marginHorizontal: 50,
   },
   bottomSection: {
     width: '100%',
