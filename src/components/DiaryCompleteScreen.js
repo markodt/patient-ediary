82d24dfeb8de8b1f44d53e2338ca6919406fc392
Marko D. Topic <markodominik.topic@silverrailtech.com>
2020-03-04 14:10:53 +0100

    Create DiaryCompleteScreen component

--- src/components/DiaryCompleteScreen.js
+++ src/components/DiaryCompleteScreen.js
@@ -1,54 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { RadioButton, Button } from 'react-native-paper';
-import * as screens from '../../screens_en.json';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
-export default class DiaryCompleteScreen extends React.Component {
-  state = {
-    value: null,
-  };
-
-  render() {
-    const { route, navigation } = this.props;
-    const screen = screens[route.name];
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.question}>{screen.text}</Text>
-        <View style={styles.radioButtonContainer}>
-          <RadioButton.Group
-            onValueChange={value => this.setState({ value })}
-            value={this.state.value}
-          >
-            <RadioButton.Item
-              label="Yes"
-              value="yes"
-              style={styles.radioButton}
-            />
-            <RadioButton.Item
-              label="No"
-              value="no"
-              style={styles.radioButton}
-            />
-          </RadioButton.Group>
-        </View>
-        <View style={styles.bottomSection}>
-          <Button
-            mode="contained"
-            uppercase={false}
-            disabled={!this.state.value}
-            onPress={() =>
-              navigation.navigate(
-                this.state.value === 'yes' ? screen.nextYes : screen.nextNo,
-              )
-            }
-          >
-            Next
-          </Button>
-        </View>
+export default function DiaryCompleteScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.centerContent}>
+        <Image
+          style={styles.diaryCompleteImage}
+          source={require('../../images/diary_complete.png')}
+        />
       </View>
-    );
-  }
+      <Text style={[styles.title, styles.centerText]}>
+        Headache Diary Complete!
+      </Text>
+      <Text style={[styles.noteText, styles.centerText]}>
+        Keep up the great work. Please tap “Finish” to confirm and save these
+        answers, or tap “Back” to correct your answers.
+      </Text>
+      <View style={styles.bottomSection}>
+        <Button
+          mode="contained"
+          uppercase={false}
+          onPress={() => navigation.navigate('h0')}
+        >
+          Finish
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,19 +36,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
   },
-  question: {
-    fontSize: 18,
+  centerText: {
+    textAlign: 'center',
+  },
+  centerContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  diaryCompleteImage: {
+    width: 230,
+    height: 164,
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#212121',
-    margin: 30,
+    marginBottom: 20,
   },
-  radioButtonContainer: {
-    marginHorizontal: 30,
-    borderTopColor: '#979797',
-    borderTopWidth: 1,
-  },
-  radioButton: {
-    borderBottomColor: '#979797',
-    borderBottomWidth: 1,
+  noteText: {
+    fontSize: 16,
+    color: '#757575',
+    marginHorizontal: 50,
   },
   bottomSection: {
     width: '100%',
