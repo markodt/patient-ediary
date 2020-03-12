9c24bf1ac4a8789fb341d8b1f767bbb7e8bf4494
Marko D. Topic <markodominik.topic@silverrailtech.com>
2020-03-12 15:50:14 +0100

    Add language change functionality to the SettingsScreen component

--- src/screens/AlertsScreen.js
+++ src/screens/AlertsScreen.js
@@ -1,16 +1,17 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, Platform, StatusBar, StyleSheet } from 'react-native';
 
 export default function AlertsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Alerts Screen</Text>
+      <Text>Alerts</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,19 +1,93 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, Platform, StatusBar, StyleSheet } from 'react-native';
+import { RadioButton, Button } from 'react-native-paper';
+import { Languages, LocalizationContext } from '../localization/i18n';
 
-export default function SettingsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Settings Screen</Text>
-    </View>
-  );
+export default class SettingsScreen extends React.Component {
+  static contextType = LocalizationContext;
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      locale: this.context.locale,
+    };
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { t, locale, setLocale } = this.context;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Settings</Text>
+        <Text style={styles.subtitle}>Change Language</Text>
+        <View style={styles.radioButtonContainer}>
+          <RadioButton.Group
+            onValueChange={value => this.setState({ locale: value })}
+            value={this.state.locale}
+          >
+            {Object.entries(Languages).map(([langCode, langName]) => {
+              return (
+                <RadioButton.Item
+                  key={langCode}
+                  label={langName}
+                  value={langCode}
+                  style={styles.radioButton}
+                />
+              );
+            })}
+          </RadioButton.Group>
+        </View>
+        <View style={styles.bottomSection}>
+          <Button
+            mode="contained"
+            uppercase={false}
+            disabled={this.state.locale === locale}
+            onPress={() => {
+              setLocale(this.state.locale);
+              navigation.navigate('home');
+            }}
+          >
+            Save Settings
+          </Button>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 30,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#212121',
+    margin: 30,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#212121',
+    marginHorizontal: 30,
+    marginBottom: 30,
+  },
+  radioButtonContainer: {
+    marginHorizontal: 30,
+    borderTopColor: '#979797',
+    borderTopWidth: 1,
+  },
+  radioButton: {
+    borderBottomColor: '#979797',
+    borderBottomWidth: 1,
+  },
+  bottomSection: {
+    width: '100%',
+    borderTopColor: '#d8d8d8',
+    borderTopWidth: 1,
+    padding: 30,
+    position: 'absolute',
+    bottom: 0,
   },
 });
